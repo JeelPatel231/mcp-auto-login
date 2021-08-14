7959903350397aafd1b68d654fec236c6c07e1ba
Foxtrot47 <jjneutron@outlook.com>
2021-08-14 17:50:32 +0530

    Install mono

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 while true
 do
     user="$( tr -dc A-Za-z0-9 </dev/urandom | head -c 5 )"
-    mono MinecraftClient.exe $user "-" HyperOt.aternos.me /ping > /dev/null &
-    sleep $(( RANDOM % 10 ))
-    kill %1
-    sleep $(( $RANDOM))
+    mono MinecraftClient.exe $user "-" HyperOt.aternos.me --Main.autorespawn=true &
+    sleep $( shuf -i 1-2 -n 1 )m
+    sudo pkill -f MinecraftClient.exe
+    sleep $( shuf -i 1-2 -n 1 )m
 done
